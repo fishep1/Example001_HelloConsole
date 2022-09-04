92859421938b11bf0d1b06d736f369f55056a7f6
Nikita Pletnev <betpezeh1@yahoo.com>
2022-09-04 09:49:07 +0500

    add Example 008 Intro Method

---
+++ Example008_IntroMerhod/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+Console.WriteLine("Введите последовательно 9 чисел");
+int a1 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+int c1 = Convert.ToInt32(Console.ReadLine());
+int a2 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+int c2 = Convert.ToInt32(Console.ReadLine());
+int a3 = Convert.ToInt32(Console.ReadLine());
+int b3 = Convert.ToInt32(Console.ReadLine());
+int c3 = Convert.ToInt32(Console.ReadLine());
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2,max3);
+
+Console.WriteLine(max);
+ 
